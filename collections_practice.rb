266fe998eab3cef84c70051d8b1384bc650c5643
Paul Holmes <paulholmes1993@gmail.com>
2019-07-08 01:43:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,3 +37,13 @@ def sum_array (array)
     sum + num
   end
 end
+
+def add_s (array)
+  array.collect.with_index do |word, index|
+    if index == 1 
+      word 
+    else 
+      word + "s"
+    end
+  end
+end
