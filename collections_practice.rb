6e67b510d3fbff85b4f24f1b61e8f075c51de3f7
Paul Holmes <paulholmes1993@gmail.com>
2019-07-08 01:37:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,13 @@ end
 def sort_array_desc (array)
   array.sort {|x,y| y <=> x }
 end
+
+def sort_array_char_count (array)
+
+end
+
+def swap_elements (array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
